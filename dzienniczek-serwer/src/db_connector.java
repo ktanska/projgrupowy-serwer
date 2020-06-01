37a5d0f87932842d54e534c7d6c726bd49288859
Lucek <julianna.wichowska@gmail.com>
2020-06-01 18:57:47 +0200

    Add : registration

--- dzienniczek-serwer/src/db_connector.java
+++ dzienniczek-serwer/src/db_connector.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,7 +16,7 @@ public class db_connector {
 			e1.printStackTrace();
 		}
 		// zmienic sciezke na wlasna
-		String url = "jdbc:sqlite:C:\\Users\\kinga\\git\\repository2\\dzienniczek-serwer\\child-health.db";
+		String url = "jdbc:sqlite:C:\\Users\\Lucek\\git\\projgrupowy-serwer\\dzienniczek-serwer\\child-health.db";
 		try {
 			conn = DriverManager.getConnection(url);
 		} catch (SQLException e) {
@@ -43,24 +44,47 @@ public class db_connector {
 			  {
 			    String login = rs.getString("login");
 			    String haslo = rs.getString("password");
+			    String mail = rs.getString("mail");
 			    System.out.println(login);
 			    System.out.println(haslo);
-			    if (login.equals(log) && haslo.equals(pass)) {
+			    System.out.println(mail);
+			    if ((login.equals(log)|| mail.equals(log)) && haslo.equals(pass)) {
 			    	System.out.println("Zalogowano");
 			    	conn.close();
 			    	return true;
-			    }
-			    else {
-			    	System.out.println("Brak u¿ytkownika w bazie");
-			    	conn.close();
-			    	return false;
-			    }
-			  } while (rs.next()); // sprawdzi wszystkie wyniki
+			    }			    
+			  } while (rs.next()); // sprawdz wszystkie wyniki
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("Brak u¿ytkownika w bazie");
+	    conn.close();
+		return false;
+	}
+	
+	public boolean rejestrowanie(String login,String mail, String password) throws SQLException {
+		// TODO Auto-generated method stub
+		
+		// polaczenie 
+		Connection conn = conn();
+		PreparedStatement Pst = conn.prepareStatement("insert into Rodzic values (NULL, ?, ?, ?);");
+
+	     
+	      try {
+	    	  //st.executeUpdate("insert into Rodzic values (NULL, ?, ?, ?);");
+	    	  Pst.setString(1, login);
+	    	  Pst.setString(2, password);
+	    	  Pst.setString(3, mail);
+              Pst.execute();
+
+      			System.out.println("Za³adowano dane rejestracji");
+	    	  return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	    conn.close();
 		return false;
 	}
-		
+	
 	}
+		
